aba1b13178e9d1a312e9794b4a06230f8d33250e
BLumia <wzc782970009@qq.com>
2015-05-28 15:02:13 +0800

    第一阶段完工
    尚未完成的部分：存储设置信息

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,8 +9,17 @@ MainWindow::MainWindow(QWidget *parent) :
 
     offset_x = 5;
     offset_y = 5;
+    clickOnFrame = false;//是否点击到面板上
+    drawCanDragCover = false;
+    ui->showOptBoxBtn->setVisible(false);
 
-    this->setWindowFlags(Qt::WindowStaysOnTopHint | Qt::FramelessWindowHint | Qt::WindowSystemMenuHint | Qt::WindowMinimizeButtonHint);//无边框
+    //窗口调节大小动画
+    optSlideAnimation = new QPropertyAnimation(ui->optionBox, "geometry");
+    optSlideAnimation->setDuration(600);
+    optSlideAnimation->setEasingCurve(QEasingCurve::OutCirc);
+
+    //this->setWindowFlags(Qt::WindowStaysOnTopHint | Qt::FramelessWindowHint | Qt::WindowSystemMenuHint | Qt::WindowMinimizeButtonHint);//无边框
+    this->setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);// | Qt::Tool
     this->setAttribute(Qt::WA_TranslucentBackground, true);//背景透明（需要上一行配合）
     timer = new QTimer(this);//控制刷新定时器
     connect(timer,SIGNAL(timeout()),this,SLOT(Update()));
@@ -31,11 +40,121 @@ void MainWindow::Update()
     int y = mouse_y - canvas_y - 5 + offset_y;// 减5由鼠标图片上边距而定
     ui->cursorLable->move(x,y);
 
-    ui->label->setText(QString::number(mouse_x)+"-"+QString::number(x));
+    ui->xposLable->setText("X ("+QString::number(mouse_x)+","+QString::number(x)+")");
+    ui->yposLable->setText("Y ("+QString::number(mouse_y)+","+QString::number(y)+")");
 }
 
 MainWindow::~MainWindow()
 {
     delete timer;
     delete ui;
+}
+
+void MainWindow::on_xoffsetSpinBox_valueChanged(int arg1)
+{
+    offset_x = arg1;
+}
+
+void MainWindow::on_yoffsetSpinBox_valueChanged(int arg1)
+{
+    offset_y = arg1;
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton)
+    {
+        drag_pos = event->pos();
+        clickOnFrame = true;//点击到窗口框架上才会变成true，改动之后新增功能：判断点击是否在边框右侧
+        event->accept();
+    }
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent *)
+{
+    clickOnFrame = false;//弹起鼠标按键时，恢复
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
+{
+    if (event->buttons() & Qt::LeftButton && clickOnFrame)
+    {
+        move(event->globalPos() - drag_pos);
+        event->accept();
+    }
+}
+
+void MainWindow::on_moveCanvasCheckr_clicked(bool checked)
+{
+    drawCanDragCover = checked;
+    this->repaint();
+}
+
+void MainWindow::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
+    if (drawCanDragCover)
+    {
+        painter.setBrush(QBrush(QColor(255, 255, 255, 120)));
+        painter.setPen(Qt::NoPen);
+        painter.drawRect(0, 0, width(), height());
+    }
+}
+
+void MainWindow::doHideOptBox(bool doHide)
+{
+    QRect startRect, endRect;
+    if (doHide) {
+        ui->showOptBoxBtn->setVisible(true);
+        startRect = QRect(ui->optionBox->geometry().x(),
+                          ui->optionBox->geometry().y(),
+                          ui->optionBox->geometry().width(),
+                          ui->optionBox->geometry().height());
+        endRect = QRect(this->geometry().width() - ui->optionBox->geometry().width() - 15,
+                        0 - ui->optionBox->geometry().height(),
+                        ui->optionBox->geometry().width(),
+                        ui->optionBox->geometry().height());
+    } else {
+        ui->showOptBoxBtn->setVisible(false);
+        startRect = QRect(this->geometry().width() - ui->optionBox->geometry().width() - 15,
+                          0 - ui->optionBox->geometry().height(),
+                          ui->optionBox->geometry().width(),
+                          ui->optionBox->geometry().height());
+        endRect = QRect(this->geometry().width() - ui->optionBox->geometry().width() - 15,
+                        0,
+                        ui->optionBox->geometry().width(),
+                        ui->optionBox->geometry().height());
+    }
+    optSlideAnimation->stop();
+    optSlideAnimation->setStartValue(startRect);
+    optSlideAnimation->setEndValue(endRect);
+    optSlideAnimation->start();
+    this->repaint();
+}
+
+void MainWindow::on_exitBtn_clicked()
+{
+    qApp->exit();//主窗口关闭后，关闭程序
+}
+
+void MainWindow::on_hideOptBoxBtn_clicked()
+{
+    doHideOptBox(true);
+}
+
+void MainWindow::on_showOptBoxBtn_clicked()
+{
+    doHideOptBox(false);
+}
+
+void MainWindow::on_canvasSizeBtn_clicked()
+{
+    if (!isMaximized()) {
+        QWidget::showMaximized();
+        this->move(0,0);
+    } else
+        QWidget::showNormal();
+
+    ui->optionBox->move(QPoint(this->geometry().width() - ui->optionBox->geometry().width() - 15,15));
+    ui->showOptBoxBtn->move(QPoint(this->geometry().width()- ui->showOptBoxBtn->geometry().width() - 15,0));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -16,15 +16,42 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    void doHideOptBox(bool doHide);
+
 private:
     Ui::MainWindow *ui;
 
     QTimer * timer;
     int offset_x;
     int offset_y;
+    bool clickOnFrame;
+    bool drawCanDragCover;//绘制可被拖拽的覆盖
+    QPoint drag_pos;//用于窗口拖动，存储鼠标坐标
+
+    //动画
+    QPropertyAnimation *optSlideAnimation;
+
 
 private slots:
     void Update();
+
+    void on_xoffsetSpinBox_valueChanged(int arg1);
+    void on_yoffsetSpinBox_valueChanged(int arg1);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *);
+    void on_moveCanvasCheckr_clicked(bool checked);
+
+    void on_exitBtn_clicked();
+
+    void on_hideOptBoxBtn_clicked();
+
+    void on_showOptBoxBtn_clicked();
+
+    void on_canvasSizeBtn_clicked();
+
+protected:
+    void paintEvent(QPaintEvent *);
 
 };
 
